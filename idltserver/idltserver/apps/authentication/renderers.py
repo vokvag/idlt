6d0b25e1272281f00252456513f75c243acf3fcf
Hao <vokvag@hotmail.com>
2018-08-14 22:28:25 +1000

    better JSON output format

---
+++ idltserver/idltserver/apps/authentication/renderers.py
@@ -0,0 +1,12 @@
+from idltserver.apps.core.renderers import IdltJSONRenderer
+
+class UserJSONRenderer(IdltJSONRenderer):
+    object_label = 'user'
+
+    def render(self, data, media_type=None, renderer_context=None):
+        token = data.get('token', None)
+
+        if token is not None and isinstance(token, bytes):
+            data['token'] = token.decode('utf-8')
+
+        return super(UserJSONRenderer, self).render(data)
