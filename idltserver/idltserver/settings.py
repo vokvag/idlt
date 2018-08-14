a86697103a3386d5e674d6640d15e740e772d04c
Hao <vokvag@hotmail.com>
2018-08-14 20:49:37 +1000

    change to postgresql

--- idltserver/idltserver/settings.py
+++ idltserver/idltserver/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/2.1/ref/settings/
 """
 
 import os
+import psycopg2.extensions
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
@@ -42,10 +43,6 @@ INSTALLED_APPS = [
     'django_extensions',
     'rest_framework',
 
-    'idltserver.apps.articles',
-    'idltserver.apps.authentication'
-    'idltserver.apps.core',
-    'idltserver.apps.profiles',
 ]
 
 MIDDLEWARE = [
@@ -84,8 +81,12 @@ WSGI_APPLICATION = 'idltserver.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': 'idlt',
+        'USER': 'idltadmin',
+        'PASSWORD': 'idolikethis',
+        'HOST': 'localhost',
+        'PORT': '',
     }
 }
 
