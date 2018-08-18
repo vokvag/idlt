31f5b499379ac01de9eb1e5d3aca29fc1775fe16
Hao <vokvag@hotmail.com>
2018-08-18 16:22:13 +1000

    change the url

--- idltserver/idltserver/apps/articles/urls.py
+++ idltserver/idltserver/apps/articles/urls.py
@@ -1,9 +1,11 @@
 from django.conf.urls import url
 
 from .views import (
-    CategoryAPIView
+    CategoryAPIView, ArticleViewSet
 )
 
 urlpatterns = [
-    url(r'^category/?$', CategoryAPIView.as_view()),
+    # url(r'^category/(?P<category_id>[-\d]+)/?$', CategoryAPIView.as_view()),
+    url(r'^articles/?$', CategoryAPIView.as_view()),
+    # url(r'^category/(?P<subcategory_id>[-\d]+)/lang/(?P<prolang_id>[-\d]+)/?$', ArticleViewSet.as_view()),
 ]
