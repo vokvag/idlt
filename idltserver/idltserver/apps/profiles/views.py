075068eed0cd9ef2c99ddfd8142deb2023c07515
Hao <vokvag@hotmail.com>
2018-08-18 16:42:24 +1000

    create profile views

---
+++ idltserver/idltserver/apps/profiles/views.py
@@ -0,0 +1,36 @@
+from rest_framework import serializers, status
+from rest_framework.exceptions import NotFound
+from rest_framework.generics import RetrieveAPIView
+from rest_framework.permissions import AllowAny, IsAuthenticated
+from rest_framework.response import Response
+from rest_framework.views import APIView
+
+from .models import Profile
+from .renderers import ProfileJSONRenderer
+from .serializers import ProfileSerializer
+
+
+class ProfileRetrieveAPIView(RetrieveAPIView):
+    permission_classes = (AllowAny,)
+    queryset = Profile.objects.select_related('user')
+    renderer_classes = (ProfileJSONRenderer,)
+    serializer_class = ProfileSerializer
+
+    def retrieve(self, request, username, *args, **kwargs):
+        # Try to retrieve the requested profile and throw an exception if the 
+        # profiel could not be found.
+        try:
+            # we use the `select_related` method to avoid making unnecessary
+            # database calls.
+            profile = self.queryset.get(
+                user__username=username
+            )
+        except Profile.DoesNotExist:
+            raise NotFound('A profile with this username does not exist.')
+
+        serializer = self.serializer_class(profile, context={
+            'request': request
+        })
+
+        return Response(serializer.data, status=status.HTTP_200_OK)
+
