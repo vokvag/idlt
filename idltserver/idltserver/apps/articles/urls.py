894c4f12a9e30b4c95d67a080afead638c4b0e0a
Hao <vokvag@hotmail.com>
2018-08-23 16:27:51 +1000

    add categories url

--- idltserver/idltserver/apps/articles/urls.py
+++ idltserver/idltserver/apps/articles/urls.py
@@ -1,12 +1,13 @@
 from django.conf.urls import url
 
 from .views import (
-    CategoryAPIView, ArticleViewSet, ProgrammingLanguageAPIView
+    CategoryAPIView, ArticleAPIView, ProgrammingLanguageAPIView
 )
 
 urlpatterns = [
     # url(r'^category/(?P<category_id>[-\d]+)/?$', CategoryAPIView.as_view()),
-    url(r'^articles/?$', CategoryAPIView.as_view()),
+    url(r'^articles/?$', ArticleAPIView.as_view()),
+    url(r'^categories/?$', CategoryAPIView.as_view()),
     url(r'^prolangs/?$', ProgrammingLanguageAPIView.as_view()),
     # url(r'^category/(?P<subcategory_id>[-\d]+)/lang/(?P<prolang_id>[-\d]+)/?$', ArticleViewSet.as_view()),
 ]
