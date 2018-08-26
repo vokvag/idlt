125cf7488e8ff477a2a1fb8429928ee5158b70d8
Hao <vokvag@hotmail.com>
2018-08-26 10:25:06 +1000

    add simple css build layout

--- idltfrontend/src/components/Article/index.js
+++ idltfrontend/src/components/Article/index.js
@@ -1,5 +1,3 @@
-//import ArticleMeta from './ArticleMeta';
-//import CommentContainer from './CommentContainer';
 import React from 'react';
 import agent from '../../agent';
 import { connect } from 'react-redux';
@@ -7,12 +5,14 @@ import { Link } from 'react-router-dom';
 import marked from 'marked';
 import { 
     ARTICLE_PAGE_LOADED, 
-    ARTICLE_PAGE_UNLOADED
+    ARTICLE_PAGE_UNLOADED,
+    CATEGORY_CHANGED
 } from '../../constants/actionTypes';
 
 import Category from './Category';
 import PlSelect from './PlSelect';
 import ArticleView from './ArticleView';
+import './Article.css';
 
 const mapStateToProps = state => ({
     ...state.article,
@@ -24,10 +24,22 @@ const mapDispatchToProps = dispatch => ({
         dispatch({ type: ARTICLE_PAGE_LOADED, payload, firstpl, secondpl }),
     onUnload: () =>
         dispatch({ type: ARTICLE_PAGE_UNLOADED }),
+    onCategoryChanged: (payload)=>
+        dispatch({type: CATEGORY_CHANGED, payload})
 });
 
 
 class Article extends React.Component {
+    componentWillReceiveProps(nextprops){
+        if(this.props.match.params.nameslug!==nextprops.match.params.nameslug){
+            if(nextprops.match.params.nameslug){
+                this.props.onCategoryChanged(
+                    agent.Articles.plArticle(nextprops.match.params.nameslug,this.props.firstpl,this.props.secondpl)
+                )
+            }
+        }
+    }
+
     componentWillMount(){
         let firstpl = window.localStorage.getItem('firstpl');
         if(!firstpl){
@@ -61,69 +73,28 @@ class Article extends React.Component {
         if(!this.props.categories){
             return null;
         }
-        // if (!this.props.article) {
-        //     return null;
-        // }
-
-        // const markup = { __html: marked(this.props.article.body, { sanitize: true })};
-        // const canModify = this.props.currentUser &&
-        //     this.props.currentUser.username === this.props.article.author.username;
-        // return (
-        //     <div className="article-page">
-        //         <div className="banner">
-        //             <div className="container">
-        //                 <h1>{this.props.article.title}</h1>
-        //                 <ArticleMeta
-        //                     article={this.props.article}
-        //                     canModify={canModify} />
-        //             </div>
-        //         </div>
-        //         <div className="container page">
-        //             <div className="row article-content">
-        //                 <div className="col-xs-12">
-        //                     <div dangerouslySetInnerHTML={markup}></div>
-        //                     <ul className="tag-list">
-        //                         {this.props.article.tagList.map(tag => {
-        //                             return (
-        //                                 <li className="tag-default tag-pill tag-outline"
-        //                                     key={tag}>
-        //                                     {tag}    
-        //                                 </li>
-        //                             );
-        //                         })}
-        //                     </ul>
-        //                 </div>
-        //             </div>
-        //             <hr/>
-        //             <div className="article-actions"></div>
-        //             <div className="row">
-        //                 <CommentContainer
-        //                     comments={this.props.comments || []}
-        //                     errors={this.props.commentErrors}
-        //                     slug={this.props.match.params.id}
-        //                     currentUser={this.props.currentUser} />
-        //             </div>
-        //         </div>
-        //     </div>
-        // );
         return(
-            <div>
-                <PlSelect 
-                    prolangs={this.props.prolangs}
-                    firstpl={this.props.firstpl}
-                    secondpl={this.props.secondpl}
-                    nameslug={this.props.match.params.nameslug}/>
-                <Category 
-                    categories={this.props.categories} 
-                    firstpl={this.props.firstpl}
-                    secondpl={this.props.secondpl}
-                    nameslug={this.props.match.params.nameslug}/>
-                <ArticleView
-                    firstpl={this.props.firstpl}
-                    secondpl={this.props.secondpl}
-                    nameslug={this.props.match.params.nameslug}
-                    articles={this.props.articles}/>
-
+            <div className="page-body">
+                <div className="nav-side-menu col-2">
+                    <PlSelect 
+                        prolangs={this.props.prolangs}
+                        firstpl={this.props.firstpl}
+                        secondpl={this.props.secondpl}
+                        nameslug={this.props.match.params.nameslug}/>
+                
+                    <Category 
+                        categories={this.props.categories} 
+                        firstpl={this.props.firstpl}
+                        secondpl={this.props.secondpl}
+                        nameslug={this.props.match.params.nameslug}/>
+                </div>
+                <div className="article-view col-10 offset-2">
+                    <ArticleView
+                        firstpl={this.props.firstpl}
+                        secondpl={this.props.secondpl}
+                        nameslug={this.props.match.params.nameslug}
+                        articles={this.props.articles}/>
+                </div>
             </div>
         );
     }
