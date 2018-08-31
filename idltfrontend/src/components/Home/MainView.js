06589b3b1e65528941592711534b2f1dd9b17e12
Hao <vokvag@hotmail.com>
2018-08-31 20:33:13 +1000

    add demo mainview

---
+++ idltfrontend/src/components/Home/MainView.js
@@ -0,0 +1,35 @@
+import React from 'react';
+
+const title = "Sorry, the site is still underdeveloping. This beta version is for deployment tests."
+
+const content = "Please bookmark my site and I won't let you disappointed."
+
+const MainView = () =>{
+    return (
+        <div className="home-sections">
+            <h1>{title}</h1>
+            <br/>
+            <br/>
+            <br/>
+            <p>{content}</p>
+            <br/>
+            <br/>
+            <br/>
+            <br/>
+            <br/>
+            <br/>
+            <br/>
+            <br/>
+            <br/>
+            <br/>
+            <br/>
+            <br/>
+            <br/>
+            <br/>
+            <br/>
+
+        </div>
+    );
+}
+
+export default MainView
