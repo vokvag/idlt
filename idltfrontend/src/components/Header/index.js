3edbd132a81634c8abea68cbdfe87276be4e4617
Hao <vokvag@hotmail.com>
2018-08-31 20:34:54 +1000

    change header

--- idltfrontend/src/components/Header/index.js
+++ idltfrontend/src/components/Header/index.js
@@ -2,10 +2,11 @@ import React from 'react';
 import { Link } from 'react-router-dom';
 import './Header.css';
 
+
 const LoggedOutView = props => {
     if (!props.currentUser) {
         return (
-            <ul>
+            <ul className="navbar-nav ml-auto">
                 <li className="nav-item">
                     <Link to="/login" className="nav-link">
                         Sign in
@@ -25,22 +26,12 @@ const LoggedOutView = props => {
 const LoggedInView = props => {
     if (props.currentUser) {
         return (
-            <ul>
-                <li className="nav-item">
-                    <Link to="/editor" className="nav-link">
-                        <i className="ion-compose"></i>&nbsp;New Post
-                    </Link>
-                </li>
-                <li className="nav-item">
-                    <Link to="" className="nav-link">
-                        <i className="ion-gear-a"></i>&nbsp;Settings
-                    </Link>
-                </li>
+            <ul className="navbar-nav ml-auto">
                 <li className="nav-item">
                     <Link 
                         to={`/@${props.currentUser.username}`} 
                         className="nav-link">
-                        <img src={props.currentUser.image} className="user-pic" alt={props.currentUser.username}/>
+                        <img src={props.currentUser.image} className="user-pic"/>
                         {props.currentUser.username}    
                     </Link>
                 </li>
@@ -54,12 +45,17 @@ class Header extends React.Component {
     render() {
         return(
             <header className="header">
-                <Link to="/" className="navbar-brand">
-                    {this.props.appName}
-                </Link>
-                <nav className="nav">                   
-                    <LoggedOutView currentUser={this.props.currentUser}></LoggedOutView>
-                    <LoggedInView currentUser={this.props.currentUser}></LoggedInView>
+                <nav className="navbar navbar-expand-md navbar-dark bg-dark">
+                    <Link to="/" className="navbar-brand">
+                        {this.props.appName}.xyz
+                    </Link>            
+                    <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarCollapse" aria-controls="navbarCollapse" aria-expanded="false" aria-label="Toggle navigation">
+                        <span className="navbar-toggler-icon"></span>
+                    </button>
+                    <div className="collapse navbar-collapse" id="navbarCollapse">       
+                        <LoggedOutView currentUser={this.props.currentUser}></LoggedOutView>
+                        <LoggedInView currentUser={this.props.currentUser}></LoggedInView>
+                    </div>
                 </nav>
             </header>
         );
