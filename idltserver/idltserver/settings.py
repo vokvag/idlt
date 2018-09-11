50540421251a16054a4afaf1204f47528be119a3
Hao <vokvag@hotmail.com>
2018-09-11 10:59:34 +1000

    add static root

--- idltserver/idltserver/settings.py
+++ idltserver/idltserver/settings.py
@@ -153,6 +153,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/2.1/howto/static-files/
 
 STATIC_URL = '/static/'
+STATIC_ROOT = os.path.join(BASE_DIR, 'static/')
 
 CORS_ORIGIN_WHITELIST = (
     '0.0.0.0:4000',
